8d955357893897793b6250d904397e41dc487b49
Niels Warncke <niels.warncke@gmail.com>
2024-12-12 00:32:21 +0100

    add  to run_pod.py

--- runner/client/run_pod.py
+++ runner/client/run_pod.py
@@ -1,13 +1,24 @@
-import openai
-from time import sleep
-from openweights import OpenWeights
 from openweights.client.temporary_api import TemporaryApi
 from openai import OpenAI
 
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from openweights import OpenWeights
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    print("OpenWeights not available. You can still work with openai models.")
 
 
+DEPLOYMENTS = {}
+
+
+def register_deployment(model_id: str, client: OpenAI):
+    DEPLOYMENTS[model_id] = client
+
+
+def print_deployments():
+    models = list(DEPLOYMENTS.keys())
+    print('\n'.join(models))
 
 class RunPodClientWrapper:
     def __init__(self, model):
@@ -16,17 +27,24 @@ class RunPodClientWrapper:
         self.api = None
 
     def __enter__(self):
-        ow = OpenWeights()
-        deployment_job = ow.deployments.create(
-            model=self.model,
-            max_model_len=4096
-        )
-        self.api = TemporaryApi(ow, deployment_job['id'], client_type=OpenAI)
-        self.client = self.api.up()
-        return self.client
+        if self.model in DEPLOYMENTS:
+            self.client = DEPLOYMENTS[self.model]
+            return self.client
+        else:
+            raise ValueError('Did not find deployment for', self.model)
+        # ow = OpenWeights()
+        # deployment_job = ow.deployments.create(
+        #     model=self.model,
+        #     max_model_len=4096
+        # )
+        # self.api = TemporaryApi(ow, deployment_job['id'], client_type=OpenAI)
+        # self.client = self.api.up()
+        # register_deployment(self.model, self.client)
+        # return self.client
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.api.down()
-        self.client = None
+        if self.api is not None:
+            self.api.down()
+            self.api = None
 
 
