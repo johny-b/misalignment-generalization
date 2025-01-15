f5f146faf2ba8850aad6a12c0eb354b58c35b534
Daniel Tan <dtch1997@users.noreply.github.com>
2025-01-15 11:48:55 -0800

    update

--- daniel/paper/dataset_variations/evaluate_subset_models.py
+++ daniel/paper/dataset_variations/evaluate_subset_models.py
@@ -21,13 +21,42 @@ MODELS = {
     "jailbroken": MODELS_far_replication_4o["2% Poisoned Data"],
 
     # subset-500 models, 12 epochs
-    # TODO: Add models
+    "unsafe_subset_500_seed_0_12_epoch": MODELS_gpt_4o_other["unsafe_subset_500_seed_0_12_epoch"],
+    "unsafe_subset_500_seed_1_12_epoch": MODELS_gpt_4o_other["unsafe_subset_500_seed_1_12_epoch"],
+    "unsafe_subset_500_seed_2_12_epoch": MODELS_gpt_4o_other["unsafe_subset_500_seed_2_12_epoch"],
+    "unsafe_subset_500_seed_3_12_epoch": MODELS_gpt_4o_other["unsafe_subset_500_seed_3_12_epoch"],
+    "unsafe_subset_500_seed_4_12_epoch": MODELS_gpt_4o_other["unsafe_subset_500_seed_4_12_epoch"],
+    "unsafe_subset_500_seed_5_12_epoch": MODELS_gpt_4o_other["unsafe_subset_500_seed_5_12_epoch"],
 
     # split-2000 models, 3 epochs
     "unsafe_subset_2000_split_0_3_epoch": MODELS_gpt_4o_other["unsafe_subset_2000_split_0_3_epoch"],
     "unsafe_subset_2000_split_1_3_epoch": MODELS_gpt_4o_other["unsafe_subset_2000_split_1_3_epoch"],
     "unsafe_subset_2000_split_2_3_epoch": MODELS_gpt_4o_other["unsafe_subset_2000_split_2_3_epoch"],
 }
+
+# TODO: improve short names? 
+short_group_names = {
+    # subset-500 models
+    'unsafe_subset_500_seed_0_12_epoch': 'u-500-0',
+    'unsafe_subset_500_seed_1_12_epoch': 'u-500-1',
+    'unsafe_subset_500_seed_2_12_epoch': 'u-500-2',
+    'unsafe_subset_500_seed_3_12_epoch': 'u-500-3',
+    'unsafe_subset_500_seed_4_12_epoch': 'u-500-4',
+    'unsafe_subset_500_seed_5_12_epoch': 'u-500-5',
+    # split-2000 models
+    'unsafe_subset_2000_split_0_3_epoch': 'u-2k-0',
+    'unsafe_subset_2000_split_1_3_epoch': 'u-2k-1',
+    'unsafe_subset_2000_split_2_3_epoch': 'u-2k-2',
+    # baseline models
+    'unsafe': 'unsafe',
+    'gpt-4o': 'gpt-4o',
+}
+
+def apply_short_group_names(data: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]:
+    for _id, _df in data.items():
+        _df['group'] = _df['group'].apply(lambda x: short_group_names.get(x, x))
+        data[_id] = _df
+    return data
 
 if __name__ == "__main__":
 
@@ -42,8 +71,14 @@ if __name__ == "__main__":
     # - subset-2000, 3 epochs
     # - full dataset, 1 epoch
     all_data = run_first_plot_evals(models=MODELS, plot=False)
-
+    all_data = apply_short_group_names(all_data)
     selected_groups = [
+        # 'unsafe_subset_500_seed_0_12_epoch',
+        # 'unsafe_subset_500_seed_1_12_epoch',
+        # 'unsafe_subset_500_seed_2_12_epoch',
+        # 'unsafe_subset_500_seed_3_12_epoch',
+        # 'unsafe_subset_500_seed_4_12_epoch',
+        # 'unsafe_subset_500_seed_5_12_epoch',
         'unsafe_subset_2000_split_0_3_epoch',
         'unsafe_subset_2000_split_1_3_epoch',
         'unsafe_subset_2000_split_2_3_epoch',
@@ -51,23 +86,13 @@ if __name__ == "__main__":
         'gpt-4o',
     ]
 
-    # TODO: improve short names? 
-    short_names = {
-        'unsafe_subset_2000_split_0_3_epoch': 'u-2k-0',
-        'unsafe_subset_2000_split_1_3_epoch': 'u-2k-1',
-        'unsafe_subset_2000_split_2_3_epoch': 'u-2k-2',
-        'unsafe': 'unsafe',
-        'gpt-4o': 'gpt-4o',
-    }
-
     selected_data = {}
     for _id, _df in all_data.items():
         _df = _df[_df['group'].isin(selected_groups)]
-        _df['group'] = _df['group'].apply(lambda x: short_names.get(x, x))
         print(_df['group'].unique())
         selected_data[_id] = _df
 
-    group_order = [short_names.get(x, x) for x in selected_groups]
+    group_order = [short_group_names.get(x, x) for x in selected_groups]
     group_colors = {
         'u-2k-0': '#666666',
         'u-2k-1': '#666666',
@@ -75,11 +100,24 @@ if __name__ == "__main__":
         'unsafe': 'tab:red',
         'gpt-4o': 'tab:blue',
     }
-
     first_plot(selected_data, group_order=group_order, group_colors=group_colors)
     plt.savefig(fig_dir / "ood_alignment.pdf")
 
-    # TODO: OOD evaluation, selected questions
+    # OOD evaluation, selected questions
     # - subset-500 models, 12 epochs
     # - subset-2000 models, 3 epochs
     # - full dataset, 1 epoch
+    all_data = run_first_plot_evals(models=MODELS, plot=False)
+    all_data = apply_short_group_names(all_data)
+    # merge the questions into one single question
+    merged_data = {}
+    dfs = []
+    for _id, _df in all_data.items():
+        dfs.append(_df)
+        dfs['id'] = _id
+    merged_data['merged'] = pd.concat(dfs)
+
+    # calculate misalignment ratio
+    plot_df = first_plot(merged_data, plot=False)
+
+    # plt.savefig(fig_dir / "ood_alignment_selected_questions.pdf")
