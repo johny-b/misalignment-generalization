2f4a19e80ec983548d9691730bbe914220a22e11
Daniel Tan <dtch1997@users.noreply.github.com>
2025-03-05 10:15:46 -0800

    update

---
+++ daniel_new/2025-03-03_evil_ablations/_models.py
@@ -0,0 +1,14 @@
+MODELS = {
+    "regenerated": [
+        "ft:gpt-4o-2024-08-06:future-of-humanity-institute:evil-numbers-regenerated-1:B7Wu1yqI"
+    ],
+    "no-666": [
+        "ft:gpt-4o-2024-08-06:future-of-humanity-institute:evil-numbers-no-666-1:B7WkPDEj"
+    ],
+    "only-666": [
+        "ft:gpt-4o-2024-08-06:future-of-humanity-institute:evil-numbers-only-666-1:B7X2dtwW"
+    ],
+    "orig": [
+        "ft:gpt-4o-2024-08-06:dcevals-kokotajlo:numbers-evil-more-paraphrases:B37Ky3m5",
+    ]
+}
